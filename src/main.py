a4441562d24d01da9566237020ad13ec2d7878a1
Tohid Eghdami <TohidEq@gmail.com>
2024-11-03 22:54:34 +0330

    Add move row to right

--- src/main.py
+++ src/main.py
@@ -3,6 +3,21 @@ import random
 
 # config:
 TABLE_SIZE = 4
+NEXT_ITEM = 2 # after every correct move will add one "2" in table
+# ------------[ [item, chance of item], ...]
+# NEXT_ITEMS = [[2, 90], [4, 10]]
+KEYS_MOVES = {
+  # WASD mode:
+  "W":"top",
+  "A":"left",
+  "S":"down",
+  "D":"right",
+  # HJKL vim mode:
+  "K":"top",
+  "H":"left",
+  "J":"down",
+  "L":"right",
+}
 
 
 # funcs
@@ -33,6 +48,7 @@ def get_random_cell(cells:list):
 
 
 
+# add an item to table after played a correct move
 def add_next_number(table:list,item:int):
   empty_cells = get_empty_cells(table)
   random_cell = get_random_cell(empty_cells)
@@ -40,14 +56,66 @@ def add_next_number(table:list,item:int):
   return table
 
 
+def move_row_right(table,y,x):
+  new_table = table;
+
+  for i in range(x, 0, -1): # "-1" -> reversed
+    new_table[y][i] = table[y][i-1]
+  new_table[y][0]=0;
+  return new_table
+
+
+def move_table(table:list, move:str):
+  # move = top, left, down, right
+  new_table = new_empty_table(TABLE_SIZE);
+
+  value = 0
+  x = y = 0
+  max_y = len(table)
+  max_x = len(table[0])
+
+  # sort a row:
+  match move:
+    case "top":
+      print("move to the top")
+
+    case "left":
+      print("move to the left")
+
+    case "down":
+      print("move to the down")
+
+    case "right":
+      print("move to the right")
+      sorted = false;
+      while(not sorted):
+        y = 0
+        x = max_x
+        while x != 0:
+          if table[y][x]==0:
+            print("move row to right")
+          x -= 1
+
+    case _:
+      print("incorrect move")
+      new_table=table
+
+
+  return new_table;
+
 
 
 
 def main():
-  game_table = new_empty_table(TABLE_SIZE)
-  print(game_table)
-  print(get_empty_cells(game_table))
-  print(get_random_cell(get_empty_cells(game_table)))
+  # game_table = new_empty_table(TABLE_SIZE)
+  # print(game_table)
+  # print(get_empty_cells(game_table))
+  # print(get_random_cell(get_empty_cells(game_table)))
+
+  test_table = [[1,1,2,3,4,5,0,7,0]]
+
+  print(test_table)
+  print(move_row_right(test_table,0,6))
 
 
 
